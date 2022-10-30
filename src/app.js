cad61a54796d1dfca4ef9443a26b780166ef7b1e
JulioCBC13 <blandojulio@gmail.com>
2022-10-30 18:22:16 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let exten = [".com", ".net", ".us", ".io", ".dev"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+pronoun.forEach(pronombres => {
+  adj.forEach(adjetivo => {
+    noun.forEach(sustantivo => {
+      exten.forEach(extensiones => {
+        console.log(`${pronombres}${adjetivo}${sustantivo}${extensiones}`);
+      });
+    });
+  });
+});
